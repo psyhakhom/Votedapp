6a8c3448cdfdde7c73fc08e16e088d20377eff92
siameseguy <psyhakhom@gmail.com>
2018-06-17 21:30:39 -0400

    Feature(Vote.js):Styling voting component

--- src/components/Vote/Vote.jsx
+++ src/components/Vote/Vote.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Grid, Form, Button} from 'semantic-ui-react'
+import {Grid, Form, Button, Header, Image} from 'semantic-ui-react'
 
 export default class Vote extends Component {
   constructor(props) {
@@ -23,6 +23,7 @@ export default class Vote extends Component {
   render() {
     return (
       <div>
+        <Header textAlign='center' size='huge'>Vote Dapp</Header>
         <Grid centered columns={2}>
           <Grid.Column>
             <Form onSubmit={this.handleSubmit}>
@@ -50,8 +51,28 @@ export default class Vote extends Component {
                   positive>Vote</Button>
               </Form.Group>
             </Form>
-
           </Grid.Column>
+        </Grid>
+        <Grid textAlign='center' centered columns={2}>
+          <Grid.Column>
+            <div className="img-container">
+              <Image
+                src="https://media.licdn.com/dms/image/C4E03AQFYLnnLTZEkTw/profile-displayphoto-shrink_800_800/0?e=1534982400&amp;v=beta&amp;t=aYuwJC3BmTOQd24rS0Ftf-n6dIJjJzne-kDFTff4lfY"
+                circular/>
+            </div>
+            <div className="img-container">
+              <Image
+                src="https://media.licdn.com/dms/image/C5603AQEXRikwj4Eslw/profile-displayphoto-shrink_800_800/0?e=1534982400&v=beta&t=6iuQviE_fRejTkDzmhqkEM6KYNINuFjiKSikGmdZeCk"
+                circular/>
+            </div>
+
+            <div className="img-container">
+              <Image
+                src="https://media.licdn.com/dms/image/C5603AQG_AGHWKpWAZg/profile-displayphoto-shrink_800_800/0?e=1534982400&v=beta&t=Kv2NETZLdRbvmhPpV-TeDaFSXLLAf_g9NVZEXevcA0A"
+                circular/>
+            </div>
+          </Grid.Column>
+
         </Grid>
 
       </div>
